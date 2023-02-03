0c49acfd5001dd94f8fa34a1de8a66d2efa2fe78
Eliel <elielpdutra.dev@gmail.com>
2023-02-03 19:29:40 -0300

    Iniciando em JS

---
+++ javascript/aula03/ex/index.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Eliel". Estou aprendendo JavaScript às', 14 ,'horas da tarde');
---
+++ javascript/aula03/index.js
@@ -0,0 +1,5 @@
+console.log("Eliel 'Porto");
+console.log('Eliel "Porto"');
+console.log(`'Eliel "Porto"`);
+
+console.log(10, 32.39, 'Eliel');
