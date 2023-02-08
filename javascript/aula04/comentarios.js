9268620c9df425aea1b761408a3442205a3c3447
Eliel <elielpdutra.dev@gmail.com>
2023-02-07 22:03:32 -0300

    Comentários

---
+++ javascript/aula04/comentarios.js
@@ -0,0 +1,12 @@
+/*
+Um bloco de código 
+Linha de comentário
+*/
+
+// Escrever um comentário
+console.log('Olá mundo');
+console.log('linha 1');
+//console.log('linha 2');
+console.log('linha 3');
+console.log('linha 4');
+//vai ser ignorado
