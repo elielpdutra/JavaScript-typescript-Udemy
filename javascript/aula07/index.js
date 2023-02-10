ea09a85e614f2bda89801ec1a6d6ad752cb7393e
Eliel <elielpdutra.dev@gmail.com>
2023-02-10 17:16:07 -0300

    Variáveis: Let e const

---
+++ javascript/aula07/index.js
@@ -0,0 +1,15 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST. 
+
+
+// String = Text | Number = Numero 
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
