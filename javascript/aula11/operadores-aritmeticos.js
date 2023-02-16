9d5e2bc843b8885ca54a4a6c395a3095f43f4736
Eliel <elielpdutra.dev@gmail.com>
2023-02-16 09:09:54 -0300

    Operadores aritméticos, de atribuição e incremento

---
+++ javascript/aula11/operadores-aritmeticos.js
@@ -0,0 +1,25 @@
+/*
+     Aritméticos
+    + Adição / concatenação
+    -  Subtração
+    /  Divisão
+    * Multiplicação
+    ** Potenciação
+    %  Resto da dvisão
+
+        Precedência
+             ()
+             **
+             * / %
+             + -
+--------------------------------
+    Incremento = ++
+    Decremento = --
+*/
+// NaN - Not a Number
+// converter string para numbeer = parseInt, parseFloat, Number
+
+const num1 = 5;
+const num2 = Number('Eliel');
+console.log(num2 - num2);
+console.log(typeof num2);
